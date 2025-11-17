f73cbad6462810bc9028fce446460a6ab4474217
Nadine CLAVERE <nadine.clavere@equadex.net>
2025-11-17 11:56:24 +0100

    Ajout du projet WPF MVVM

---
+++ Nimbus2025Wpf/Services/HttpClientBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+namespace Nimbus2025Wpf.Services
+{
+    abstract class HttpClientBase
+    {
+        private const string baseAddress = "https://localhost:7069/api/";
+        private HttpClient client = null!;
+        protected HttpClient Client
+        {
+            get
+            {
+                if (client == null)
+                {
+                    client = new HttpClient();
+                    client.BaseAddress = new Uri(baseAddress);
+                }
+                return client;
+            }
+        }
+    }
+}
---
+++ Nimbus2025Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Nimbus2025Wpf.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+
+		private ICommand commandAeroports = null!;
+		public ICommand CommandAeroports
+		{
+			get 
+			{ 
+				if(commandAeroports == null)
+				{
+					commandAeroports = new RelayCommand(
+                        o =>
+                        {
+                            //corpd de la méthode exécutée par la commande
+
+
+                        });
+                }
+
+				return commandAeroports; 
+			}
+		}
+
+		
+
+	}
+}
