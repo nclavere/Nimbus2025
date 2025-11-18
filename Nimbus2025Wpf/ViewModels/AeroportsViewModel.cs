5f1340c5b9bb0df0a3986fa428398959471d86d2
Nadine CLAVERE <nadine.clavere@equadex.net>
2025-11-18 09:49:23 +0100

    finalisation de le vm de liste des aeroports

--- Nimbus2025Wpf/ViewModels/AeroportsViewModel.cs
+++ Nimbus2025Wpf/ViewModels/AeroportsViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using Nimbus2025Transverse.Dtos;
 using Nimbus2025Wpf.Services;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace Nimbus2025Wpf.ViewModels;
 internal class AeroportsViewModel : ViewModelBase
 {
 
     public ObservableCollection<AirportDto> Aeroports { get; set; } = null!;
+    public ICollectionView Observer { get; set; }
+
+    public string SelectedAeroportDetails { 
+        get
+        {
+            return Observer?.CurrentItem != null ? 
+                ((AirportDto)Observer.CurrentItem).Name 
+                : "Aucun aéroport sélectionné";
+        }
+    }
 
     public AeroportsViewModel()
     {
@@ -23,14 +35,41 @@ internal class AeroportsViewModel : ViewModelBase
         aeroports.ForEach(a => Aeroports.Add(a));
 
         //3. on pose un observateur
+        Observer = CollectionViewSource.GetDefaultView(Aeroports);
+
+        Observer.SortDescriptions.Add(
+            new SortDescription(nameof(AirportDto.Code), ListSortDirection.Descending)
+        );
 
         //4. on veut être notifié du changement de sélection coté vue
+        Observer.CurrentChanged += (sender, e) =>
+        {
+            NotifyPropertyChanged(nameof(SelectedAeroportDetails));
+        };        
 
         //5. on notifie la vue que la liste a changé
         NotifyPropertyChanged(nameof(Aeroports));
+        Observer.MoveCurrentToLast();
     }
 
-
+    public string Filtre
+    {
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Observer.Filter = null;
+            }
+            else
+            {
+                Observer.Filter = obj =>
+                {
+                    AirportDto aeroport = (AirportDto)obj;
+                    return aeroport.Name.Contains(value, StringComparison.InvariantCultureIgnoreCase);
+                };
+            }
+        }
+    }
 
 
 }
